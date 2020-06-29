19a80dd1486c2ba36a66e7a2c9ad40d56aa2eae6
zhang <zhangchaopear@163.com>
2020-06-29 16:36:12 +0800

    v0.12

--- src/main/java/cn/enjoy/controller/NewController.java
+++ src/main/java/cn/enjoy/controller/NewController.java
@@ -2,6 +2,6 @@ package cn.enjoy.controller;
 
 public class NewController {
     public static void main(String[] args) {
-        System.out.println("v0.8 -合并分支-push git 111  github");
+        System.out.println("v0.8 -合并分支-push git 111  github vo.1222");
     }
 }
